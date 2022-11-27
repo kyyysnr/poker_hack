75a71a4e130eb29e6eeb257e1578fe0bb161b415
EC2 Default User <73773480+kyyysnr@users.noreply.github.com>
2022-11-27 14:49:41 +0000

    非同期化通信

--- app/controllers/dislikes_controller.rb
+++ app/controllers/dislikes_controller.rb
@@ -3,12 +3,13 @@ class DislikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @dislike = Dislike.new(post_id: params[:post_id], ip: request.remote_ip)
     @dislike.save
-    redirect_back(fallback_location: root_path)
+
   end
 
   def destroy
+    @post = Post.find(params[:post_id])
     @dislike = Dislike.find_by(params[:post_id], ip:request.remote_ip)
     @dislike.destroy
-    redirect_back(fallback_location: root_path)
+
   end
 end
