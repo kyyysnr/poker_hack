48852eaf9fe6e973890b071f869791dcb78ac6ca
EC2 Default User <73773480+kyyysnr@users.noreply.github.com>
2022-11-27 06:48:03 +0000

    レイアウト

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   has_many :comment_likes,dependent: :destroy
   has_many :comment_dislikes,dependent: :destroy
+  has_many_attached :images
 
   validates :body, presence:true
 
