ee53d4149f36705c3eab37e5a53c5fc028236e2e
EC2 Default User <73773480+kyyysnr@users.noreply.github.com>
2022-11-28 12:23:25 +0000

    非同期化通信その２

--- app/controllers/comment_likes_controller.rb
+++ app/controllers/comment_likes_controller.rb
@@ -1,13 +1,15 @@
 class CommentLikesController < ApplicationController
   def create
+    @comment = Comment.find_by(id: params[:comment_id])
     @comment_like = CommentLike.new(comment_id: params[:comment_id], ip: request.remote_ip)
     @comment_like.save
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    @comment = Comment.find_by(id: params[:comment_id])
     @comment_like = CommentLike.find_by(comment_id: params[:comment_id], ip: request.remote_ip)
     @comment_like.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path)
   end
 end
